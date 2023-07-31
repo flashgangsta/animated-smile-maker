a789013510b42e87f717e04d06f014212f821db7
Sergey Krivtsov <flashgangsta@gmail.com>
2023-07-31 02:51:58 +0200

    Removed old sources

--- _old/js/app/interface/panels/PanelButtonRemove.js
+++
@@ -1,9 +0,0 @@
-import {PanelButton} from "./PanelButton.js";
-
-export class PanelButtonRemove extends PanelButton {
-	constructor() {
-		super("./assets/remove-layer-ico.png");
-	}
-}
-
-customElements.define("panel-button-remove", PanelButtonRemove);
