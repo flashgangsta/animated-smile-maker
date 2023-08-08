97c108b2ce13d09c59c2c861648dbb704b81f6b6
Sergey Krivtsov <flashgangsta@gmail.com>
2023-08-08 16:39:25 +0200

    Library remove button functionality

--- src/widgets/library/LibraryItemsList.ts
+++ src/widgets/library/LibraryItemsList.ts
@@ -1,6 +1,7 @@
 import {SubPanel} from "../../features/components/sub_panel/SubPanel.js";
-import {Events} from "../../shared/lib/Events.js";
+import {Events} from "../../shared/lib/Events";
 import {PanelButtonRemove} from "../../entities/components/panel_button/PanelButtonRemove.js";
+import {EventListener} from "../../shared/utils/EventListener.js";
 
 export class LibraryItemsList extends SubPanel {
     constructor() {
@@ -9,16 +10,29 @@ export class LibraryItemsList extends SubPanel {
 
         const removeButton:PanelButtonRemove = new PanelButtonRemove();
 
-        removeButton.addEventListener(Events.CLICK, (event:Event) => {
+        this.listenEvents(
+            new EventListener(removeButton, Events.CLICK, (event: Event): void => {
+                //TODO: Move this and same in TimelineLayers to extends class
 
-
-        });
+                const selectedItem:Element | null = this.selectedItem;
+                if(selectedItem) {
+                    const children:HTMLCollection = this.subPanelContainer.children;
+                    const index:number = Array.from(children).indexOf(selectedItem);
+                    selectedItem.dispatchEvent(new Event(Events.LIBRARY_ITEM_REMOVE, {bubbles: true}));
+                    selectedItem.remove();
+                    if(children.length) {
+                        (children[Math.min(index, children.length-1)] as HTMLElement).click();
+                    }
+                    this.dispatchEvent(new Event(Events.LIBRARY_ITEM_REMOVED, {bubbles: true}));
+                }
+            }),
+        );
 
         this.footer.append(removeButton);
     }
 
 
-    get selectedItem() {
+    get selectedItem(): Element | null {
         return this.subPanelContainer.querySelector(".selected");
     }
 
