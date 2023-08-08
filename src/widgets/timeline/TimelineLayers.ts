2715344dca513b773b6bc6222e5c957571c285bc
Sergey Krivtsov <flashgangsta@gmail.com>
2023-08-08 19:33:30 +0200

    Styling Timeline Layers

--- src/widgets/timeline/TimelineLayers.ts
+++ src/widgets/timeline/TimelineLayers.ts
@@ -1,8 +1,39 @@
 import {SubPanel} from "../../features/components/sub_panel/SubPanel.js";
+import {PanelButton} from "../../entities/components/panel_button/PanelButton.js";
+import {PanelButtonRemove} from "../../entities/components/panel_button/PanelButtonRemove.js";
+import {Events} from "../../shared/lib/Events";
 
 export class TimelineLayers extends SubPanel {
     constructor() {
         super();
+        this.id = "timeline-layers";
+        this.init();
+    }
+
+
+    private init(): void {
+        const buttonVisibility: PanelButton = new PanelButton("eye-ico.png");
+        const buttonLock: PanelButton = new PanelButton("lock-ico.png");
+
+        const buttonNewLayer = new PanelButton("new-layer-ico.png");
+        const buttonRemoveLayer: PanelButtonRemove = new PanelButtonRemove();
+
+        //this.addLayer();
+
+        //buttonNewLayer.addEventListener(Events.CLICK, (event) => this.addLayer());
+        //buttonRemoveLayer.addEventListener(Events.CLICK, (event) => this.removeSelectedLayer());
+        //this.projectConfig.addEventListener(Events.PROJECT_OPEN, (event) => this.#onProjectOpen());
+        //this.projectConfig.addEventListener(Events.PROJECT_LAYERS_INIT, (event) => this.#loadProjectLayers());
+
+        this.subPanelContainer.classList.add("layers-container");
+
+        /*this.subPanelContainer.addEventListener(Events.LAYER_SELECT, (event) => {
+            event.stopPropagation();
+            this.getSelectedLayer()?.unselect();
+        });*/
+
+        this.header.append(buttonVisibility, buttonLock);
+        this.footer.append(buttonNewLayer, buttonRemoveLayer);
     }
 }
 
